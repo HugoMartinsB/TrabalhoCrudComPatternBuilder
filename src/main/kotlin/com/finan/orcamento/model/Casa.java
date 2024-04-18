1446ca0bbe406ecd086621bec054866493766b7d
HugoMartinsB <164739348+HugoMartinsB@users.noreply.github.com>
2024-04-18 19:12:14 -0300

    Funciona a tabela e o teste, falta crud

--- src/main/kotlin/com/finan/orcamento/model/Casa.java
+++ src/main/kotlin/com/finan/orcamento/model/Casa.java
@@ -1,38 +1,48 @@
 package com.finan.orcamento.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import org.jetbrains.annotations.NotNull;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="Casas")
 public class Casa implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
-    @Column(name="num_quarto")
-    private BigDecimal quartos;
+    @Column(name="quatos_casa")
+    public BigDecimal quartos;
 
-    @NotNull
-    @Column(name="num_banheiros")
-        private BigDecimal banheiros;
+    @Column(name="banheiros_casa")
+    public BigDecimal banheiros;
 
-    @NotNull
-    @Column(name="churrasqueira")
-    private boolean churrasqueira;
+    @Column(name="churrasqueiras_casa")
+    public BigDecimal churrasqueira;
 
-    public Casa(){};
-
-    public Casa(int quartos, int banheiros, boolean churrasqueira) {
-        this.quartos = BigDecimal.valueOf(quartos);
+    public Casa(int quartos, int banheiros, int churrasqueira){
         this.banheiros = BigDecimal.valueOf(banheiros);
-        this.churrasqueira = churrasqueira;
-
+        this.quartos = BigDecimal.valueOf(quartos);
+        this.churrasqueira = BigDecimal.valueOf(churrasqueira);
     }
-}
 
+    public BigDecimal getQuartos() {
+        return quartos;
+    }
+
+    public BigDecimal getBanheiros() {
+        return banheiros;
+    }
+
+    public BigDecimal isChurrasqueira() {
+        return churrasqueira;
+    }
+
+}
